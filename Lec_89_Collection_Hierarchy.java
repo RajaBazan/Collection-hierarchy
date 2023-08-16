20266b243735ebabbb5d571652862414e5674928
Muhammad Bazan <112274805+RajaBazan@users.noreply.github.com>
2023-08-17 02:14:42 +0500

    Add files via upload

---
+++ Lec_89_Collection_Hierarchy.java
@@ -0,0 +1,12 @@
+package hello.com;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class Lec_89_Collection_Hierarchy {
+    public static void main(String[] args) {
+//        ArrayList
+//        TreeSet
+
+    }
+}
